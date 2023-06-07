d4b7956edf3ccd30ec7a3c5c50b9720fdfddfa2f
valentine <valentinetuyrin@gmail.com>
2023-06-07 12:19:30 +0300

    no message

---
+++ app/src/main/java/com/example/myapplication/ui/notifications/AccountFragment.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication.databinding.FragmentAccountBinding
+
+class AccountFragment : Fragment() {
+
+    private var _binding: FragmentAccountBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        val accountViewModel = ViewModelProvider(this)[AccountViewModel::class.java]
+
+        _binding = FragmentAccountBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textNotifications
+        accountViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ui/search/SearchFragment.kt
@@ -0,0 +1,39 @@
+package com.example.myapplication.ui.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication.databinding.FragmentSearchBinding
+
+
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val searchViewModel = ViewModelProvider(this)[SearchViewModel::class.java]
+
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textSearch
+        searchViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
